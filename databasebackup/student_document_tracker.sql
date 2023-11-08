4570604a8cacbfb5faf646bcec005f797e16db94
armanduallo123 <150100757+armanduallo123@users.noreply.github.com>
2023-11-08 11:14:56 +0800

    databasebackup

---
+++ databasebackup/student_document_tracker.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 08, 2023 at 04:11 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_document_tracker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_background`
+--
+
+CREATE TABLE `student_background` (
+  `student_id` int(11) NOT NULL,
+  `student_name` varchar(255) NOT NULL,
+  `course` varchar(255) NOT NULL,
+  `age` varchar(255) NOT NULL,
+  `year_level` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `section` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student_background`
+--
+ALTER TABLE `student_background`
+  ADD PRIMARY KEY (`student_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
